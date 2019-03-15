2c5298bb5a12986a238a0695b86d6d771accb714
kdas <kaustav.das@evolving.com>
2019-03-15 13:04:47 +0200

    .

--- evolution/src/main/resources/bin/evolution-deploy-launch-middle.sh
+++ evolution/src/main/resources/bin/evolution-deploy-launch-middle.sh
@@ -13,7 +13,7 @@ docker stack deploy -c $DEPLOY_ROOT/stack/stack-upgrade.yml ${DOCKER_STACK}-upgr
 echo waiting for upgrade to complete ...
 UPGRADE_EXECUTING=1
 while [ $UPGRADE_EXECUTING -ne 0 ]; do
-  ( docker logs `docker service ps | grep ${DOCKER_STACK}-upgrade | cut -d' ' -f1` 2>&1 ) | grep -q "Created /evolving/nglm/upgraded"
+  ( docker logs `docker container ps | grep ${DOCKER_STACK}-upgrade | cut -d' ' -f1` 2>&1 ) | grep -q "Created /evolving/nglm/upgraded"
   UPGRADE_EXECUTING=$?
   if [ $UPGRADE_EXECUTING -ne 0 ]; then
     echo upgrade executing ...
