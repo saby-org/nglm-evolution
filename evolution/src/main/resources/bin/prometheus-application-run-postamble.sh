b40eb35a3733fb54fe705d358c7421e0080864a8
kdas <kaustav.das@evolving.com>
2019-12-31 11:12:58 +0200

    .

--- evolution/src/main/resources/bin/prometheus-application-run-postamble.sh
+++ evolution/src/main/resources/bin/prometheus-application-run-postamble.sh
@@ -8,4 +8,4 @@
 #  run
 #
 
-exec /bin/prometheus -web.listen-address=${MONITORING_HOST}:${PROMETHEUS_APPLICATION_PORT} -alertmanager.url=http://${MONITORING_HOST}:${ALERTMANAGER_PORT} -storage.local.retention=${STORAGE_LOCAL_RETENTION} -storage.local.target-heap-size=${STORAGE_LOCAL_TARGET_HEAP_SIZE} -config.file=/etc/prometheus/prometheus-application.yml -storage.local.path=/prometheus -web.console.libraries=/usr/share/prometheus/console_libraries -web.console.templates=/usr/share/prometheus/consoles
+exec /bin/prometheus --web.listen-address=${MONITORING_HOST}:${PROMETHEUS_APPLICATION_PORT} --config.file=/etc/prometheus/prometheus-application.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention.time=${STORAGE_LOCAL_RETENTION} --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
