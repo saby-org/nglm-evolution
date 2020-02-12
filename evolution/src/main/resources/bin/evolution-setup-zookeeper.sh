936240db88464de8f48776f920b00c77142c0fe3
Frédéric Duclos <frederic.duclos@lumatagroup.com>
2020-02-12 10:35:11 +0100

    EVPRO-49

--- evolution/src/main/resources/bin/evolution-setup-zookeeper.sh
+++ evolution/src/main/resources/bin/evolution-setup-zookeeper.sh
@@ -1,21 +1,21 @@
-  #################################################################################
-  #
-  #  evolution-setup-zookeeper
-  #
-  #################################################################################
+#################################################################################
+#
+#  evolution-setup-zookeeper
+#
+#################################################################################
 
-  #
-  #  subscriberGroup
-  #
+#
+#  subscriberGroup
+#
   
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups "subscriberGroups"
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups/epochs "subscriberGroupEpochs"
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups/locks "subscriberGroupLocks"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups "subscriberGroups"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups/epochs "subscriberGroupEpochs"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/subscriberGroups/locks "subscriberGroupLocks"
 
-  #
-  #  stock
-  #
+#
+#  stock
+#
 
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock "stock"
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock/stocks "stocks"
-  $ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock/stockmonitors "stockMonitors"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock "stock"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock/stocks "stocks"
+$ZOOKEEPER_SHELL $ZOOKEEPER_SERVERS create ${zookeeper.root}/stock/stockmonitors "stockMonitors"
