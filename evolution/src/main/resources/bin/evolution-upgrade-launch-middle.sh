b6651e93c333e5258b82ce06a451ef86559c05b9
Frédéric Duclos <frederic.duclos@lumatagroup.com>
2020-02-19 14:26:00 +0100

    .

--- evolution/src/main/resources/bin/evolution-upgrade-launch-middle.sh
+++ evolution/src/main/resources/bin/evolution-upgrade-launch-middle.sh
@@ -4,21 +4,3 @@
 #
 #########################################
 
-docker stack deploy -c $DEPLOY_ROOT/stack/stack-upgrade.yml ${DOCKER_STACK}-upgrade
-
-#
-#  wait for upgrade to complete
-#
-
-echo waiting for upgrade to complete ...
-UPGRADE_EXECUTING=1
-while [ $UPGRADE_EXECUTING -ne 0 ]; do
-  ( docker service logs `docker service ls | grep ${DOCKER_STACK}-upgrade | cut -d' ' -f1` 2>&1 ) | grep -q "Created /evolving/nglm/upgraded"
-  UPGRADE_EXECUTING=$?
-  if [ $UPGRADE_EXECUTING -ne 0 ]; then
-    echo upgrade executing ...
-    sleep 5
-  fi
-done
-echo upgrade complete
-
