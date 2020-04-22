c14cec726c3e4cacddd1ced5ca3fb38f46e0934d
Marc Kossa <marc.kossa@evolving.com>
2020-04-22 11:02:03 +0200

    align with 1.4.2

--- evolution/src/main/java/com/evolving/nglm/evolution/PresentationLog.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/PresentationLog.java
@@ -6,32 +6,22 @@
 
 package com.evolving.nglm.evolution;
 
-import com.evolving.nglm.core.ConnectSerde;
-import com.evolving.nglm.core.SchemaUtilities;
-import com.evolving.nglm.core.SubscriberStreamEvent;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-import com.rii.utilities.JSONUtilities;
-import com.rii.utilities.JSONUtilities.JSONUtilitiesException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-import org.apache.kafka.common.errors.SerializationException;
-import org.apache.kafka.common.serialization.Serde;
-import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.data.Timestamp;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
+import com.evolving.nglm.core.ConnectSerde;
+import com.evolving.nglm.core.SchemaUtilities;
+import com.evolving.nglm.core.SubscriberStreamEvent;
+import com.rii.utilities.JSONUtilities;
 
 public class PresentationLog implements SubscriberStreamEvent
 {
@@ -50,7 +40,7 @@ public class PresentationLog implements SubscriberStreamEvent
   {
     SchemaBuilder schemaBuilder = SchemaBuilder.struct();
     schemaBuilder.name("presentation_log");
-    schemaBuilder.version(SchemaUtilities.packSchemaVersion(3));
+    schemaBuilder.version(SchemaUtilities.packSchemaVersion(4));
     schemaBuilder.field("msisdn", Schema.STRING_SCHEMA);
     schemaBuilder.field("subscriberID", Schema.STRING_SCHEMA);
     schemaBuilder.field("eventDate", Schema.INT64_SCHEMA);
@@ -71,6 +61,7 @@ public class PresentationLog implements SubscriberStreamEvent
     schemaBuilder.field("balance", Schema.OPTIONAL_FLOAT64_SCHEMA);
     schemaBuilder.field("moduleID", Schema.OPTIONAL_STRING_SCHEMA);
     schemaBuilder.field("featureID", Schema.OPTIONAL_STRING_SCHEMA);
+    schemaBuilder.field("presentationDates", SchemaBuilder.array(Timestamp.SCHEMA).optional().schema());
     schemaBuilder.field("tokenTypeID", Schema.OPTIONAL_STRING_SCHEMA);
     schema = schemaBuilder.build();
   };
@@ -115,6 +106,7 @@ public class PresentationLog implements SubscriberStreamEvent
   private Double balance;
   private String moduleID;
   private String featureID;
+  private List<Date> presentationDates;
   private String tokenTypeID;
 
   /****************************************
@@ -143,6 +135,7 @@ public class PresentationLog implements SubscriberStreamEvent
   public Double getBalance() { return balance; }
   public String getModuleID() { return moduleID; }
   public String getFeatureID() { return featureID; }
+  public List<Date> getPresentationDates() { return presentationDates;}
   public String getTokenTypeID() { return tokenTypeID; }
 
   /*****************************************
@@ -151,7 +144,7 @@ public class PresentationLog implements SubscriberStreamEvent
   *
   *****************************************/
 
-  public PresentationLog(String msisdn, String subscriberID, Date eventDate, String callUniqueIdentifier, String channelID, String salesChannelID, String userID, String presentationToken, String presentationStrategyID, Integer transactionDurationMs, List<String> offerIDs, List<Double> offerScores, List<Integer> positions,   String controlGroupState, List<String> scoringStrategyIDs, String retailerMsisdn, Double rechargeAmount, Double balance, String moduleID, String featureID, String tokenTypeID)
+  public PresentationLog(String msisdn, String subscriberID, Date eventDate, String callUniqueIdentifier, String channelID, String salesChannelID, String userID, String presentationToken, String presentationStrategyID, Integer transactionDurationMs, List<String> offerIDs, List<Double> offerScores, List<Integer> positions,   String controlGroupState, List<String> scoringStrategyIDs, String retailerMsisdn, Double rechargeAmount, Double balance, String moduleID, String featureID, List<Date> presentationDates, String tokenTypeID)
   {
     this.msisdn = msisdn;
     this.subscriberID = subscriberID;
@@ -173,6 +166,7 @@ public class PresentationLog implements SubscriberStreamEvent
     this.balance = balance;
     this.moduleID = moduleID;
     this.featureID = featureID;
+    this.presentationDates = presentationDates;
     this.tokenTypeID = tokenTypeID;
 }
 
@@ -202,13 +196,34 @@ public class PresentationLog implements SubscriberStreamEvent
     this.offerScores = decodeOfferScores(JSONUtilities.decodeJSONArray(jsonRoot, "offerScores", true));
     this.positions = decodePositions(JSONUtilities.decodeJSONArray(jsonRoot, "positions", true));
     this.controlGroupState = JSONUtilities.decodeString(jsonRoot, "controlGroupState", true);
-    this.scoringStrategyIDs = decodeScoringStrategyIDs(JSONUtilities.decodeJSONArray(jsonRoot, "scoringStrategyIDs", true));
+    this.scoringStrategyIDs = decodeScoringStrategyIDs(JSONUtilities.decodeJSONArray(jsonRoot, "scoringStrategyIDs", false));
     this.retailerMsisdn = JSONUtilities.decodeString(jsonRoot, "retailerMsisdn", false);
     this.rechargeAmount = JSONUtilities.decodeDouble(jsonRoot, "rechargeAmount", false);
     this.balance = JSONUtilities.decodeDouble(jsonRoot, "balance", false);
     this.moduleID = JSONUtilities.decodeString(jsonRoot, "moduleID", false);
     this.featureID = JSONUtilities.decodeString(jsonRoot, "featureID", false);
+    this.presentationDates = decodePresentationDates(JSONUtilities.decodeJSONArray(jsonRoot, "presentationDates", false));
     this.tokenTypeID = JSONUtilities.decodeString(jsonRoot, "tokenTypeID", false);
+  }
+
+  /*****************************************
+  *
+  *  decodePresentationDates
+  *
+  *****************************************/
+
+  private List<Date> decodePresentationDates(JSONArray jsonArray)
+  {
+    List<Date> dates = new ArrayList<>();
+    if (jsonArray != null)
+      {
+        for (int i=0; i<jsonArray.size(); i++)
+          {
+            String date = (String) jsonArray.get(i);
+            dates.add(GUIManagedObject.parseDateField(date));
+          }
+      }
+    return dates;
   }
 
   /*****************************************
@@ -323,6 +338,7 @@ public class PresentationLog implements SubscriberStreamEvent
     struct.put("balance", presentationLog.getBalance());
     struct.put("moduleID", presentationLog.getModuleID());
     struct.put("featureID", presentationLog.getFeatureID());
+    struct.put("presentationDates", presentationLog.getPresentationDates());
     struct.put("tokenTypeID", presentationLog.getTokenTypeID());
    return struct;
   }
@@ -374,12 +390,13 @@ public class PresentationLog implements SubscriberStreamEvent
     Double balance = valueStruct.getFloat64("balance");
     String moduleID = (schemaVersion >= 2) ? valueStruct.getString("moduleID") : null;
     String featureID = (schemaVersion >= 2) ? valueStruct.getString("featureID") : null;
-    String tokenTypeID = (schemaVersion >= 3) ? valueStruct.getString("tokenTypeID") : null;
-    
+    List<Date> presentationDates = (schemaVersion >= 3) ? (List<Date>)valueStruct.get("presentationDates") : new ArrayList<Date>();
+    String tokenTypeID = (schemaVersion >= 4) ? valueStruct.getString("tokenTypeID") : null;
     //
     //  return
     //
 
-    return new PresentationLog(msisdn, subscriberID, eventDate, callUniqueIdentifier, channelID, salesChannelID, userID, presentationToken, presentationStrategyID, transactionDurationMs, offerIDs, offerScores, positions, controlGroupState, scoringStrategyIDs, retailerMsisdn, rechargeAmount, balance, moduleID, featureID, tokenTypeID);
+    return new PresentationLog(msisdn, subscriberID, eventDate, callUniqueIdentifier, channelID, salesChannelID, userID, presentationToken, presentationStrategyID, transactionDurationMs, offerIDs, offerScores, positions, controlGroupState, scoringStrategyIDs, retailerMsisdn, rechargeAmount, balance, moduleID, featureID, presentationDates, tokenTypeID);
   }
+  
 }
