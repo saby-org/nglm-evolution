ca01bf39a0525d50eb3d6a28cb266c9bef6ee70d
Raj K <rajkamal.mitra@evolving.com>
2020-05-12 15:08:51 +0530

    extra fields

--- evolution/src/main/java/com/evolving/nglm/evolution/SubscriberRelatives.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/SubscriberRelatives.java
@@ -20,9 +20,13 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.json.simple.JSONObject;
 
+import com.evolving.nglm.core.AlternateID;
 import com.evolving.nglm.core.ConnectSerde;
 import com.evolving.nglm.core.JSONUtilities;
+import com.evolving.nglm.core.ReferenceDataReader;
 import com.evolving.nglm.core.SchemaUtilities;
+import com.evolving.nglm.core.SystemTime;
+import com.evolving.nglm.evolution.SubscriberProfileService.SubscriberProfileServiceException;
 
 public class SubscriberRelatives
 {
@@ -121,19 +125,62 @@ public class SubscriberRelatives
     }
   
   /*****************************************
-  *
-  *  getJSONRepresentation
-  *
-  *****************************************/
+   *
+   * getJSONRepresentation
+   *
+   *****************************************/
+
+  public JSONObject getJSONRepresentation(String relationshipID, SubscriberProfileService subscriberProfileService, ReferenceDataReader<String, SubscriberGroupEpoch> subscriberGroupEpochReader)
+  {
+    HashMap<String, Object> json = new HashMap<String, Object>();
     
-  public JSONObject getJSONRepresentation(String relationshipID)
-    {
-      HashMap<String,Object> json = new HashMap<String,Object>();
-      json.put("relationshipID", relationshipID);
-      json.put("parentSubscriberID", getParentSubscriberID());
-      json.put("childrenSubscriberIDs", JSONUtilities.encodeArray(new ArrayList<String>(getChildrenSubscriberIDs())));
-      return JSONUtilities.encodeObject(json);
-    }
+    //
+    //  obj
+    //
+    
+    json.put("relationshipID", relationshipID);
+    json.put("relationshipName", Deployment.getSupportedRelationships().get(relationshipID) != null ? Deployment.getSupportedRelationships().get(relationshipID).getName() : null);
+    
+    //
+    //  parent
+    //
+    
+    HashMap<String, Object> parentJson = new HashMap<String, Object>();
+    try
+      {
+        SubscriberProfile parentProfile = subscriberProfileService.getSubscriberProfile(getParentSubscriberID());
+        SubscriberEvaluationRequest evaluationRequest = new SubscriberEvaluationRequest(parentProfile, subscriberGroupEpochReader, SystemTime.getCurrentTime());
+        parentJson.put("subscriberID", getParentSubscriberID());
+        for (String id : Deployment.getAlternateIDs().keySet())
+          {
+            AlternateID alternateID = Deployment.getAlternateIDs().get(id);
+            CriterionField criterionField = Deployment.getProfileCriterionFields().get(alternateID.getProfileCriterionField());
+            if (criterionField != null)
+              {
+                String alternateIDValue = (String) criterionField.retrieve(evaluationRequest);
+                parentJson.put(alternateID.getID(), alternateIDValue);
+              }
+          }
+        json.put("parentDetails", JSONUtilities.encodeObject(parentJson));
+      } 
+    catch (SubscriberProfileServiceException e)
+      {
+        e.printStackTrace();
+      }
+    
+    //
+    //  children
+    //
+    
+    json.put("numberOfChildren", getChildrenSubscriberIDs().size());
+    json.put("childrenSubscriberIDs", JSONUtilities.encodeArray(new ArrayList<String>(getChildrenSubscriberIDs())));
+    
+    //
+    //  result
+    //
+    
+    return JSONUtilities.encodeObject(json);
+  }
   
   /*****************************************
    *
@@ -142,13 +189,13 @@ public class SubscriberRelatives
    *****************************************/
 
   public static Object pack(Object value)
-    {
-      SubscriberRelatives hierarchy = (SubscriberRelatives) value;
-      Struct struct = new Struct(schema);
-      struct.put("parentSubscriberID", hierarchy.getParentSubscriberID());
-      struct.put("childrenSubscriberIDs", packChildrenSubscriberIDs(hierarchy.getChildrenSubscriberIDs()));
-      return struct;
-    }
+  {
+    SubscriberRelatives hierarchy = (SubscriberRelatives) value;
+    Struct struct = new Struct(schema);
+    struct.put("parentSubscriberID", hierarchy.getParentSubscriberID());
+    struct.put("childrenSubscriberIDs", packChildrenSubscriberIDs(hierarchy.getChildrenSubscriberIDs()));
+    return struct;
+  }
 
   /****************************************
    *
@@ -157,14 +204,14 @@ public class SubscriberRelatives
    ****************************************/
 
   private static List<Object> packChildrenSubscriberIDs(Set<String> childrenSubscriberIDs)
-    {
-      List<Object> result = new ArrayList<Object>();
-      for (String childID : childrenSubscriberIDs)
-        {
-          result.add(childID);
-        }
-      return result;
-    }
+  {
+    List<Object> result = new ArrayList<Object>();
+    for (String childID : childrenSubscriberIDs)
+      {
+        result.add(childID);
+      }
+    return result;
+  }
 
   /*****************************************
    *
@@ -173,38 +220,38 @@ public class SubscriberRelatives
    *****************************************/
 
   public static SubscriberRelatives unpack(SchemaAndValue schemaAndValue)
-    {
-      //
-      // data
-      //
+  {
+    //
+    // data
+    //
 
-      Schema schema = schemaAndValue.schema();
-      Object value = schemaAndValue.value();
-      Integer schemaVersion = (schema != null) ? SchemaUtilities.unpackSchemaVersion0(schema.version()) : null;
+    Schema schema = schemaAndValue.schema();
+    Object value = schemaAndValue.value();
+    Integer schemaVersion = (schema != null) ? SchemaUtilities.unpackSchemaVersion0(schema.version()) : null;
 
-      //
-      // unpack
-      //
+    //
+    // unpack
+    //
 
-      Struct valueStruct = (Struct) value;
-      String parentSubscriberID = valueStruct.getString("parentSubscriberID");
-      Set<String> childrenSubscriberIDs = unpackChildrenSubscriberIDs((List<String>) valueStruct.get("childrenSubscriberIDs"));
-      
-      //
-      // validate
-      //
+    Struct valueStruct = (Struct) value;
+    String parentSubscriberID = valueStruct.getString("parentSubscriberID");
+    Set<String> childrenSubscriberIDs = unpackChildrenSubscriberIDs((List<String>) valueStruct.get("childrenSubscriberIDs"));
 
-      if (childrenSubscriberIDs == null) 
-        {
-          childrenSubscriberIDs = new LinkedHashSet<String>();
-        }
+    //
+    // validate
+    //
 
-      //
-      // return
-      //
+    if (childrenSubscriberIDs == null)
+      {
+        childrenSubscriberIDs = new LinkedHashSet<String>();
+      }
 
-      return new SubscriberRelatives(parentSubscriberID, childrenSubscriberIDs);
-    }
+    //
+    // return
+    //
+
+    return new SubscriberRelatives(parentSubscriberID, childrenSubscriberIDs);
+  }
   
   /*****************************************
    *
@@ -213,13 +260,13 @@ public class SubscriberRelatives
    *****************************************/
 
   private static Set<String> unpackChildrenSubscriberIDs(List<String> childrenSubscriberIDs)
-    {
-      Set<String> result = new LinkedHashSet<String>();
-      for (String childID : childrenSubscriberIDs)
-        {
-          result.add(childID);
-        }
-      return result;
-    }
+  {
+    Set<String> result = new LinkedHashSet<String>();
+    for (String childID : childrenSubscriberIDs)
+      {
+        result.add(childID);
+      }
+    return result;
+  }
 
 }
