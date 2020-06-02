874ac37cc775030c4ae52eff677e5294206a55dc
Raj K <rajkamal.mitra@evolving.com>
2020-06-02 15:21:48 +0530

    compile

--- evolution/src/main/java/com/evolving/nglm/evolution/reports/journeycustomerstatistics/JourneyCustomerStatisticsReportCsvWriter.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/reports/journeycustomerstatistics/JourneyCustomerStatisticsReportCsvWriter.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.zip.ZipOutputStream;
 
 import org.slf4j.Logger;
