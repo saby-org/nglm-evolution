8ff78aee63b45d9f0615b4507fd418ea435a172f
Raj K <rajkamal.mitra@evolving.com>
2020-06-22 11:21:19 +0530

    .

--- evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
@@ -59,6 +59,8 @@ import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.streams.errors.InvalidStateStoreException;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -81,6 +83,7 @@ import org.slf4j.LoggerFactory;
 import com.evolving.nglm.core.Alarm;
 import com.evolving.nglm.core.AlternateID;
 import com.evolving.nglm.core.ConnectSerde;
+import com.evolving.nglm.core.CronFormat;
 import com.evolving.nglm.core.JSONUtilities;
 import com.evolving.nglm.core.JSONUtilities.JSONUtilitiesException;
 import com.evolving.nglm.core.LicenseChecker;
@@ -95,6 +98,7 @@ import com.evolving.nglm.core.ServerRuntimeException;
 import com.evolving.nglm.core.StringKey;
 import com.evolving.nglm.core.SubscriberIDService;
 import com.evolving.nglm.core.SubscriberIDService.SubscriberIDServiceException;
+import com.evolving.nglm.core.utilities.UtilitiesException;
 import com.evolving.nglm.core.SystemTime;
 import com.evolving.nglm.core.UniqueKeyServer;
 import com.evolving.nglm.evolution.CommodityDeliveryManager.CommodityDeliveryOperation;
@@ -600,6 +604,8 @@ public class GUIManager
   *****************************************/
 
   protected static UniqueKeyServer epochServer = new UniqueKeyServer();
+  
+  private Thread periodicEvaluatorThread = null;
 
   /*****************************************
   *
@@ -1978,6 +1984,16 @@ public class GUIManager
 
     NGLMRuntime.addShutdownHook(new ShutdownHook(kafkaProducer, restServer, dynamicCriterionFieldService, journeyService, segmentationDimensionService, pointService, offerService, scoringStrategyService, presentationStrategyService, callingChannelService, salesChannelService, sourceAddressService, supplierService, productService, catalogCharacteristicService, contactPolicyService, journeyObjectiveService, offerObjectiveService, productTypeService, ucgRuleService, deliverableService, tokenTypeService, voucherTypeService, voucherService, subscriberProfileService, subscriberIDService, subscriberGroupEpochReader, journeyTrafficReader, renamedProfileCriterionFieldReader, deliverableSourceService, reportService, subscriberMessageTemplateService, uploadedFileService, targetService, communicationChannelBlackoutService, loyaltyProgramService, resellerService, exclusionInclusionTargetService, dnboMatrixService, segmentContactPolicyService, criterionFieldAvailableValuesService));
 
+    /*****************************************
+    *
+    *  periodicEvaluator
+    *
+    *****************************************/
+    
+    Runnable periodicEvaluator = new Runnable() { @Override public void run() { runPeriodicEvaluator(); } };
+    periodicEvaluatorThread = new Thread(periodicEvaluator, "GUIManager-PeriodicEvaluator");
+    periodicEvaluatorThread.start();
+    
     /*****************************************
     *
     *  log restServerStarted
@@ -20977,6 +20993,382 @@ private JSONObject processGetOffersList(String userID, JSONObject jsonRoot) thro
         keySerializer.serialize(topic, new StringKey(subscriberID)),
         valueSerializer.serialize(topic, tokenChange)
         ));
+  }
+  
+  /*****************************************
+  *
+  *  runPeriodicEvaluator
+  *
+  *****************************************/
+
+  private void runPeriodicEvaluator()
+  {
+    /*****************************************
+    *
+    *  abort if regression mode
+    *
+    *****************************************/
+
+    if (Deployment.getRegressionMode()) return;
+
+    /*****************************************
+    *
+    *  abort if periodic evaluation is not configured
+    *
+    *****************************************/
+
+    if (Deployment.getPeriodicEvaluationCronEntry() == null) return;
+
+    /*****************************************
+    *
+    *  cron
+    *
+    *****************************************/
+
+    CronFormat periodicEvaluation = null;
+    try
+      {
+        periodicEvaluation = new CronFormat(Deployment.getPeriodicEvaluationCronEntry(), TimeZone.getTimeZone(Deployment.getBaseTimeZone()));
+      }
+    catch (UtilitiesException e)
+      {
+        log.error("bad perodicEvaluationCronEntry {}", e.getMessage());
+        StringWriter stackTraceWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stackTraceWriter, true));
+        log.info(stackTraceWriter.toString());
+        return;
+      }
+
+    /*****************************************
+    *
+    *  main loop
+    *
+    *****************************************/
+
+    NGLMRuntime.registerSystemTimeDependency(this);
+    Date now = SystemTime.getCurrentTime();
+    Date nextPeriodicEvaluation = periodicEvaluation.previous(now);
+    while (true)
+      {
+        /*****************************************
+        *
+        *  wait until reload schedule is needed
+        *
+        *****************************************/
+
+        synchronized (this)
+          {
+            while (now.before(nextPeriodicEvaluation))
+              {
+                try
+                  {
+                    this.wait(nextPeriodicEvaluation.getTime() - now.getTime());
+                  }
+                catch (InterruptedException e)
+                  {
+                  }
+                now = SystemTime.getCurrentTime();
+              }
+          }
+
+        /*****************************************
+        *
+        *  log start
+        *
+        *****************************************/
+
+        log.info("guimanager daily task {} (start)", nextPeriodicEvaluation);
+        
+        /*****************************************
+        *
+        *  createRecurrentJourneys
+        *
+        *****************************************/
+
+        //createRecurrentJourneys();
+
+        /*****************************************
+        *
+        *  log end
+        *
+        *****************************************/
+
+        log.info("guimanager daily task {} (end)", nextPeriodicEvaluation);
+
+        /*****************************************
+        *
+        *  nextPeriodicEvaluation
+        *
+        *****************************************/
+
+        nextPeriodicEvaluation = periodicEvaluation.next(RLMDateUtils.addSeconds(nextPeriodicEvaluation,1));
+        log.info("guimanager daily task {} (next)", nextPeriodicEvaluation);
+      }
+  }
+  
+  /*****************************************
+  *
+  *  createRecurrentJourneys
+  *
+  *****************************************/
+  
+  private void createRecurrentJourneys()
+  {
+    log.info("creating recurrent campaigns");
+    Date now = SystemTime.getCurrentTime();
+    now = RLMDateUtils.truncate(now, Calendar.DATE, Deployment.getBaseTimeZone());
+    Collection<Journey> recurrentJourneys = journeyService.getActiveRecurrentJourneys(now);
+    for (Journey recurrentJourney : recurrentJourneys)
+      {
+        List<Date> journeyCreationDates = new ArrayList<Date>();
+        JourneyScheduler journeyScheduler = recurrentJourney.getJourneyScheduler();
+        Journey latestJourney = journeyService.getLatestRecurrentJourney(recurrentJourney.getGUIManagedObjectID());
+        
+        //
+        //  limit reached
+        //
+        
+        if (journeyScheduler.getNumberOfOccurrences() <= latestJourney.getOccurrenceNumber()) continue;
+        log.info("RAJ K creating recurrent campaign limit ok");
+        
+        //
+        //  scheduling
+        //
+        
+        String scheduling = journeyScheduler.getRunEveryUnit().toLowerCase();
+        Integer scheduligInterval = journeyScheduler.getRunEveryDuration();
+        
+        log.info("RAJ K creating recurrent campaign for {} and scheduling {} scheduligInterval {}", recurrentJourney.getJourneyID(), scheduling, scheduligInterval);
+        switch (scheduling)
+          {
+            case "week":
+              Date firstDateOfThisWk = getFirstDate(now, Calendar.DAY_OF_WEEK);
+              Date lastDateOfThisWk = getLastDate(now, Calendar.DAY_OF_WEEK);
+              
+              //
+              //  nextExpectedDate
+              //
+              
+              Date nextExpectedDate = RLMDateUtils.addWeeks(recurrentJourney.getEffectiveStartDate(), scheduligInterval, Deployment.getBaseTimeZone());
+              while (nextExpectedDate.before(firstDateOfThisWk))
+                {
+                  nextExpectedDate = RLMDateUtils.addWeeks(nextExpectedDate, scheduligInterval, Deployment.getBaseTimeZone());
+                }
+              
+              //
+              //  not in this week
+              //
+              log.info("RAJ K nextExpectedDate {} ", nextExpectedDate);
+              if (RLMDateUtils.truncate(nextExpectedDate, Calendar.DATE, Deployment.getBaseTimeZone()).after(lastDateOfThisWk)) continue;
+              log.info("RAJ K nextExpectedDate is ok");
+              //
+              //  this is the week
+              //
+              
+              List<Date> expectedCreationDates = getExpectedCreationDates(firstDateOfThisWk, lastDateOfThisWk, scheduling, journeyScheduler.getRunEveryWeekDay());
+              
+              //
+              //  journeyCreationDates
+              //
+              
+              Collection<Journey> recurrentSubJourneys = journeyService.getAllRecurrentJourneysByID(recurrentJourney.getJourneyID());
+              for (Date expectedDate : expectedCreationDates)
+                {
+                  boolean exists = false;
+                  for (Journey subJourney : recurrentSubJourneys)
+                    {
+                      exists = RLMDateUtils.truncatedCompareTo(expectedDate, subJourney.getEffectiveStartDate(), Calendar.DATE, Deployment.getBaseTimeZone()) == 0;
+                      if (exists) break;
+                    }
+                  if(!exists) journeyCreationDates.add(expectedDate);
+                }
+              break;
+              
+            case "month":
+              Date firstDateOfThisMonth = getFirstDate(now, Calendar.DAY_OF_MONTH);
+              Date lastDateOfThisMonth = getLastDate(now, Calendar.DAY_OF_MONTH);
+              
+              //
+              //  nextExpectedDate
+              //
+              
+              Date nextExpDate = RLMDateUtils.addMonths(recurrentJourney.getEffectiveStartDate(), scheduligInterval, Deployment.getBaseTimeZone());
+              while (nextExpDate.before(firstDateOfThisMonth))
+                {
+                  nextExpDate = RLMDateUtils.addMonths(nextExpDate, scheduligInterval, Deployment.getBaseTimeZone());
+                }
+              
+              //
+              //  not in this month
+              //
+              log.info("RAJ K nextExpDate {} ", nextExpDate);
+              if (RLMDateUtils.truncate(nextExpDate, Calendar.DATE, Deployment.getBaseTimeZone()).after(lastDateOfThisMonth)) continue;
+              log.info("RAJ K nextExpDate is ok");
+              //
+              //  this is the month
+              //
+              
+              List<Date> expCreationDates = getExpectedCreationDates(firstDateOfThisMonth, lastDateOfThisMonth, scheduling, journeyScheduler.getRunEveryMonthDay());
+              
+              //
+              //  journeyCreationDates
+              //
+              
+              Collection<Journey> rcrntSubJourneys = journeyService.getAllRecurrentJourneysByID(recurrentJourney.getJourneyID());
+              for (Date expectedDate : expCreationDates)
+                {
+                  boolean exists = false;
+                  for (Journey subJourney : rcrntSubJourneys)
+                    {
+                      exists = RLMDateUtils.truncatedCompareTo(expectedDate, subJourney.getEffectiveStartDate(), Calendar.DATE, Deployment.getBaseTimeZone()) == 0;
+                      if (exists) break;
+                    }
+                  if(!exists) journeyCreationDates.add(expectedDate);
+                }
+              break;
+
+            default:
+              break;
+        }
+        
+        //
+        //  createJourneys
+        //
+        
+        createJourneys(recurrentJourney, journeyCreationDates, latestJourney.getOccurrenceNumber());
+      }
+    log.info("created recurrent campaigns");
+  }
+  
+  //
+  //  createJourneys
+  //
+  
+  private void createJourneys(Journey recurrentJourney, List<Date> journeyCreationDates, Integer existingOccurrenceNumber)
+  {
+    log.info("RAJ K createingJourneys of {}, for {} existingOccurrenceNumber {}", recurrentJourney.getJourneyID(), journeyCreationDates, existingOccurrenceNumber);
+    Integer occurrenceNumber = existingOccurrenceNumber;
+    int daysBetween = RLMDateUtils.daysBetween(recurrentJourney.getEffectiveStartDate(), recurrentJourney.getEffectiveEndDate(), Deployment.getBaseTimeZone());
+    for (Date startDate : journeyCreationDates)
+      {
+        JSONObject journeyJSON = (JSONObject) journeyService.getJSONRepresentation(recurrentJourney).clone();
+        journeyJSON.put("apiVersion", 1);
+        
+        //
+        //  remove
+        //
+        
+        journeyJSON.remove("recurrence");
+        journeyJSON.remove("scheduler");
+        
+        //
+        //  add
+        //
+        
+        String journeyID = journeyService.generateJourneyID();
+        journeyJSON.put("id", journeyID);
+        journeyJSON.put("name", recurrentJourney.getGUIManagedObjectName() + "_" + occurrenceNumber);
+        journeyJSON.put("occurrenceNumber", ++occurrenceNumber);
+        journeyJSON.put("display", recurrentJourney.getGUIManagedObjectDisplay() + "_" + occurrenceNumber);
+        journeyJSON.put("effectiveStartDate", recurrentJourney.formatDateField(startDate));
+        journeyJSON.put("effectiveEndDate", recurrentJourney.formatDateField(RLMDateUtils.addDays(startDate, daysBetween, Deployment.getBaseTimeZone())));
+        
+        //
+        //  create and activate
+        //
+        
+        processPutJourney("0", journeyJSON, recurrentJourney.getGUIManagedObjectType());
+        processSetActive("0", journeyJSON, recurrentJourney.getGUIManagedObjectType(), true);
+      }
+  }
+  
+  //
+  //  getExpectedCreationDates
+  //
+  
+  private List<Date> getExpectedCreationDates(Date firstDate, Date lastDate, String scheduling, List<String> runEveryDay)
+  {
+    List<Date> result = new ArrayList<Date>();
+    while (firstDate.before(lastDate) || firstDate.compareTo(lastDate) == 0)
+      {
+        int day = -1;
+        switch (scheduling)
+          {
+            case "week":
+              day = RLMDateUtils.getField(firstDate, Calendar.DAY_OF_WEEK, Deployment.getBaseTimeZone());
+              break;
+              
+            case "month":
+              day = RLMDateUtils.getField(firstDate, Calendar.DAY_OF_MONTH, Deployment.getBaseTimeZone());
+              break;
+
+            default:
+              break;
+        }
+        String dayOf = String.valueOf(day);
+        if (runEveryDay.contains(dayOf)) result.add(new Date(firstDate.getTime()));
+        firstDate = RLMDateUtils.addDays(firstDate, 1, Deployment.getBaseTimeZone());
+      }
+    
+    //
+    //  handle last date of month
+    //
+    
+    if ("month".equalsIgnoreCase(scheduling))
+      {
+        int lastDayOfMonth = RLMDateUtils.getField(lastDate, Calendar.DAY_OF_MONTH, Deployment.getBaseTimeZone());
+        for (String day : runEveryDay)
+          {
+            if (Integer.parseInt(day) > lastDayOfMonth) result.add(new Date(lastDate.getTime()));
+          }
+      }
+    log.info("RAJ K getExpectedCreationDates {}", result);
+    return result;
+  }
+
+  //
+  //  getFirstDate
+  //
+  
+  private Date getFirstDate(Date now, int dayOf)
+  {
+    Date firstDateOfNext = RLMDateUtils.ceiling(now, dayOf, Deployment.getBaseTimeZone());
+    if (Calendar.DAY_OF_WEEK == dayOf)
+      {
+        return RLMDateUtils.addDays(firstDateOfNext, -7, Deployment.getBaseTimeZone());
+      }
+    else
+      {
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone(Deployment.getBaseTimeZone()));
+        c.setTime(now);
+        int dayOfMonth = RLMDateUtils.getField(now, Calendar.DAY_OF_MONTH, Deployment.getBaseTimeZone());
+        Date firstDate = RLMDateUtils.addDays(now, -dayOfMonth+1, Deployment.getBaseTimeZone());
+        return firstDate;
+      }
+  }
+  
+  //
+  //  getLastDate
+  //
+  
+  private Date getLastDate(Date now, int dayOf)
+  {
+    Date firstDateOfNext = RLMDateUtils.ceiling(now, dayOf, Deployment.getBaseTimeZone());
+    if (Calendar.DAY_OF_WEEK == dayOf)
+      {
+        Date firstDateOfthisWk = RLMDateUtils.addDays(firstDateOfNext, -7, Deployment.getBaseTimeZone());
+        return RLMDateUtils.addDays(firstDateOfthisWk, 6, Deployment.getBaseTimeZone());
+      }
+    else
+      {
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone(Deployment.getBaseTimeZone()));
+        c.setTime(now);
+        int toalNoOfDays = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int dayOfMonth = RLMDateUtils.getField(now, Calendar.DAY_OF_MONTH, Deployment.getBaseTimeZone());
+        Date firstDate = RLMDateUtils.addDays(now, -dayOfMonth+1, Deployment.getBaseTimeZone());
+        Date lastDate = RLMDateUtils.addDays(firstDate, toalNoOfDays-1, Deployment.getBaseTimeZone());
+        return lastDate;
+      }
   }
 
 }
--- evolution/src/main/java/com/evolving/nglm/evolution/TimerService.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/TimerService.java
@@ -658,7 +658,7 @@ public class TimerService
         *
         *****************************************/
 
-        createRecurrentJourneys();
+        //createRecurrentJourneys();
 
         /*****************************************
         *
