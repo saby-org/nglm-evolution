dbc5f440ec3df07aaa7abb6b28923ee7a1eaba26
Raj K <rajkamal.mitra@evolving.com>
2020-08-04 13:19:12 +0530

    log clean

--- evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
@@ -23274,6 +23274,7 @@ private JSONObject processGetOffersList(String userID, JSONObject jsonRoot) thro
             //  add
             //
 
+            log.info("RAJ K criterionFieldJSON {}", criterionFieldJSON);
             result.add(criterionFieldJSON);
           }
       }
