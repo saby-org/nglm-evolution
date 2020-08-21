9434d2ba4a11225436adb9a209fd98f72cf53304
Fatima <Fatima@192.168.64.244>
2020-08-21 10:52:09 +0200

    New class (TopRows.java) is added to com.evolving.nglm.evolution.reports

---
+++ evolution/src/main/java/com/evolving/nglm/evolution/reports/TopRows.java
@@ -0,0 +1,38 @@
+package com.evolving.nglm.evolution.reports;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class TopRows {
+
+	static void readFirstNLines(String inputFileName, String outputFileName, int topRows) throws Exception {
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+
+		ReadFile file = new ReadFile(inputFileName);
+		String[] aryLines = file.openFile();
+		int totalNmbrOfLines = file.readLines();
+		int i;
+		if (!(new File(inputFileName).exists())) {
+			throw new IllegalArgumentException("ERROR: The file doesn't exist!");
+		}
+
+		else if (new File(inputFileName).length() == 0) {
+			throw new IllegalArgumentException("ERROR: The file is empty!");
+		}
+
+		else if (topRows > totalNmbrOfLines || topRows <= 0) {
+			throw new IllegalArgumentException(
+					"ERROR: The number of rows to be displayed (topRows) should be 0 < topRows < totalNmbrOfLines!");
+		} else {
+			for (i = 0; i < topRows; i++) {
+				bw.write(aryLines[i]);
+				bw.write("\n");
+			}
+			bw.close();
+		}
+
+	}
+
+}
