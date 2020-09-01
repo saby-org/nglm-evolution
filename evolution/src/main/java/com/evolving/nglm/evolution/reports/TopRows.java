a878c165003706ed29e25ca2cdc6156d2fbe7621
Fatima <Fatima@192.168.65.119>
2020-09-01 11:07:34 +0200

    TopRows.java is added: it allows extracting the first N rows of the report.

--- evolution/src/main/java/com/evolving/nglm/evolution/reports/TopRows.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/reports/TopRows.java
@@ -1,38 +1,49 @@
 package com.evolving.nglm.evolution.reports;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TopRows {
+	
+	private static Logger log = LoggerFactory.getLogger(TopRows.class);
+	
+	public static void extractTopRows(String inputFile, String outputFile, int topRows) 
+	{
+		try
+		{
+			String strLine;
+			int i = 0;
+			BufferedReader br = new BufferedReader(new FileReader(inputFile));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+			while ((strLine = br.readLine()) != null) 
+			{
+				if (i++ < topRows) 
+				{
+					bw.write(strLine);
+					bw.write("\n");
+				}
+				else
+					break;
 
-	public static void readFirstNLines(String inputFileName, String outputFileName, int topRows) throws Exception {
-
-		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
-
-		ReadFile file = new ReadFile(inputFileName);
-		String[] aryLines = file.openFile();
-		int totalNmbrOfLines = file.readLines();
-		int i;
-		if (!(new File(inputFileName).exists())) {
-			throw new IllegalArgumentException("ERROR: The file doesn't exist!");
-		}
-
-		else if (new File(inputFileName).length() == 0) {
-			throw new IllegalArgumentException("ERROR: The file is empty!");
-		}
-
-		else if (topRows > totalNmbrOfLines || topRows <= 0) {
-			throw new IllegalArgumentException(
-					"ERROR: The number of rows to be displayed (topRows) should be 0 < topRows < totalNmbrOfLines!");
-		} else {
-			for (i = 0; i < topRows; i++) {
-				bw.write(aryLines[i]);
-				bw.write("\n");
 			}
+			br.close();
 			bw.close();
+
 		}
-
+		catch (FileNotFoundException e) 
+		{
+			log.error("The file doesn't exist!", e);
+		}
+		catch (IOException e) 
+		{
+			log.error("The file is empty!", e);
+		}
 	}
-
 }
