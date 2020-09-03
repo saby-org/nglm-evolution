0eee5f9c267dd29caf9de735b7eee7514f9b6e7f
Fatima <Fatima@192.168.65.153>
2020-09-03 13:57:58 +0200

    The visibility of zip and unzip methods is changed to public.

---
+++ evolution/src/main/java/com/evolving/nglm/evolution/reports/ZipFile.java
@@ -0,0 +1,30 @@
+package com.evolving.nglm.evolution.reports;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.zip.*;
+ 
+public class ZipFile {
+ 
+    public static void zipFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            String zipFileName = file.getName().concat(".zip");
+ 
+            FileOutputStream fos = new FileOutputStream(zipFileName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+ 
+            zos.putNextEntry(new ZipEntry(file.getName()));
+ 
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            zos.write(bytes, 0, bytes.length);
+            zos.closeEntry();
+            zos.close();
+ 
+        } catch (FileNotFoundException ex) {
+            System.err.format("The file %s does not exist", filePath);
+        } catch (IOException ex) {
+            System.err.println("I/O error: " + ex);
+        }
+    }
+}
