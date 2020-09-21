226d8fddca85728a5d99a28ae671251642c9b3ad
Roshnee Meena <roshnee.perumal@evolving.com>
2020-09-21 15:56:18 +0200

    .

--- evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/GUIManager.java
@@ -23203,7 +23203,9 @@ private JSONObject processGetOffersList(String userID, JSONObject jsonRoot) thro
 
                       }
                     else {
-                      if(log.isInfoEnabled()) log.info("offer is not supplierOffer");
+                      response.put("responseCode", RESTAPIGenericReturnCodes.OFFER_UNKNOWN.getGenericResponseCode());
+                      response.put("responseMessage", RESTAPIGenericReturnCodes.OFFER_UNKNOWN.getGenericResponseMessage());
+                      return JSONUtilities.encodeObject(response);
                     }
 
                   }
@@ -23379,8 +23381,9 @@ private JSONObject processGetOffersList(String userID, JSONObject jsonRoot) thro
               }
             else
               {
-                if (log.isInfoEnabled())
-                  log.info("offer is not supplierOffer");
+                response.put("responseCode", RESTAPIGenericReturnCodes.OFFER_UNKNOWN.getGenericResponseCode());
+                response.put("responseMessage", RESTAPIGenericReturnCodes.OFFER_UNKNOWN.getGenericResponseMessage());
+                return JSONUtilities.encodeObject(response);
               }
 
           }
