317c3535d7acfb515d5deda7dc0ba00b8b32f8ec
Marc Kossa <marc.kossa@evolving.com>
2020-10-14 14:19:36 +0200

    EVPRO-297 WIP

--- evolution/src/main/java/com/evolving/nglm/evolution/EvolutionEngine.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/EvolutionEngine.java
@@ -5180,7 +5180,8 @@ public class EvolutionEngine
                         String hierarchyRelationship = (String) CriterionFieldRetriever.getJourneyNodeParameter(entryActionEvaluationRequest, "node.parameter.relationship");
                         if (hierarchyRelationship != null && hierarchyRelationship.trim().equals("InternalID-Supplier"))
                           {
-                            String customerIDSupplier = XX; // Find customer ID of the supplier
+                            log.info("MK supplier");
+                            String customerIDSupplier = ""; // Find customer ID of the supplier
                             ExecuteActionOtherSubscriber action = new ExecuteActionOtherSubscriber(customerIDSupplier, entryActionEvaluationRequest.getSubscriberProfile().getSubscriberID(), entryActionEvaluationRequest.getJourneyState().getJourneyID(), entryActionEvaluationRequest.getJourneyNode().getNodeID(), context.getUniqueKey(), entryActionEvaluationRequest.getJourneyState());
                             actions.add(action);
                           }
