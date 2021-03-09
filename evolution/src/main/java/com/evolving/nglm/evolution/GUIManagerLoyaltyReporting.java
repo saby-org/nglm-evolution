d39c1fc9d3ffd480d3e863c64e2b279e7d1b97e9
Raj Kamal Mitra <rajkamal.mitra@evolving.com>
2021-03-09 22:17:32 +0530

    log

--- evolution/src/main/java/com/evolving/nglm/evolution/GUIManagerLoyaltyReporting.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/GUIManagerLoyaltyReporting.java
@@ -178,6 +178,7 @@ public class GUIManagerLoyaltyReporting extends GUIManager
 
   JSONObject processPutLoyaltyProgram(String userID, JSONObject jsonRoot, LoyaltyProgramType loyaltyProgramType)
   {
+    log.info("RAJ K processPutLoyaltyProgram jsonRoot {} loyaltyProgramType {}", jsonRoot, loyaltyProgramType);
     /****************************************
     *
     *  response
@@ -279,10 +280,12 @@ public class GUIManagerLoyaltyReporting extends GUIManager
         *  store
         *
         *****************************************/
+        log.info("RAJ K processPutLoyaltyProgram dryRun {}", dryRun);
         if (!dryRun)
           {
 
             loyaltyProgramService.putLoyaltyProgram(loyaltyProgram, (existingLoyaltyProgram == null), userID);
+            log.info("RAJ K processPutLoyaltyProgram putLoyaltyProgram {}", loyaltyProgram);
 
             /*****************************************
              *
@@ -290,6 +293,7 @@ public class GUIManagerLoyaltyReporting extends GUIManager
              *
              *****************************************/
 
+            log.info("RAJ K processPutLoyaltyProgram addLoyaltyProgramCriterionFields {}");
             dynamicCriterionFieldService.addLoyaltyProgramCriterionFields(loyaltyProgram, (existingLoyaltyProgram == null));
 
             /*****************************************
