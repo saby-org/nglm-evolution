389d8c6dabd99c280f61adfd718fb8eddc206f9f
Raj Kamal Mitra <rmitra@evolving.com>
2021-03-25 12:44:52 +0530

    levelUpdateDate in ES fix

--- evolution/src/main/resources/bin/evolution-setup-elasticsearch.sh
+++ evolution/src/main/resources/bin/evolution-setup-elasticsearch.sh
@@ -98,7 +98,8 @@ prepare-es-update-curl -XPUT http://$MASTER_ESROUTER_SERVER/_template/subscriber
               "loyaltyProgramEnrollmentDate" : { "type" : "date" },
               "loyaltyProgramExitDate"       : { "type" : "date" },
               "tierUpdateDate"               : { "type" : "date" },
-              "loyaltyProgramEpoch"          : { "type" : "long" }
+              "loyaltyProgramEpoch"          : { "type" : "long" },
+              "levelUpdateDate"              : { "type" : "date" },
            }
       },
       "pointBalances"                       : { "type": "nested",
