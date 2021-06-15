33bbb1c4b9c7b3c12c32b3ec8c39dfa450886ac5
Raj Kamal Mitra <rmitra@evolving.com>
2021-06-15 14:05:13 +0530

    .

--- evolution/src/main/java/com/evolving/nglm/evolution/PartnersESSinkConnector.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/PartnersESSinkConnector.java
@@ -1,5 +1,6 @@
 package com.evolving.nglm.evolution;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,8 +10,10 @@ import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.sink.SinkRecord;
 
 import com.evolving.nglm.core.ChangeLogESSinkTask;
+import com.evolving.nglm.core.RLMDateUtils;
 import com.evolving.nglm.core.ServerRuntimeException;
 import com.evolving.nglm.core.SimpleESSinkConnector;
+import com.evolving.nglm.core.SystemTime;
 
 public class PartnersESSinkConnector extends SimpleESSinkConnector
 {
@@ -56,7 +59,36 @@ public class PartnersESSinkConnector extends SimpleESSinkConnector
     @Override
     public Map<String, Object> getDocumentMap(GUIManagedObject item)
     {
+      Date now = SystemTime.getCurrentTime();
       Map<String,Object> documentMap = new HashMap<String,Object>();
+      if (item instanceof Supplier)
+        {
+          Supplier supplier = (Supplier) item;
+          documentMap.put("createdDate", RLMDateUtils.formatDateForElasticsearchDefault(supplier.getCreatedDate()));
+          documentMap.put("display", supplier.getGUIManagedObjectDisplay());
+          documentMap.put("active", supplier.getActive());
+          documentMap.put("partnerType", PartnerType.Supplier.name());
+          documentMap.put("id", supplier.getGUIManagedObjectID());
+          documentMap.put("parentId", supplier.getParentSupplierID());
+          documentMap.put("timestamp", RLMDateUtils.formatDateForElasticsearchDefault(now));
+        }
+      else if (item instanceof Reseller)
+        {
+          Reseller reseller = (Reseller) item;
+          documentMap.put("createdDate", RLMDateUtils.formatDateForElasticsearchDefault(reseller.getCreatedDate()));
+          documentMap.put("display", reseller.getGUIManagedObjectDisplay());
+          documentMap.put("active", reseller.getActive());
+          documentMap.put("partnerType", PartnerType.Reseller.name());
+          documentMap.put("id", reseller.getGUIManagedObjectID());
+          documentMap.put("email", reseller.getEmail());
+          documentMap.put("parentId", reseller.getParentResellerID());
+          documentMap.put("timestamp", RLMDateUtils.formatDateForElasticsearchDefault(now));
+          //documentMap.put("provider", reseller.get); RAJ K
+        }
+      else
+        {
+          throw new ServerRuntimeException("invalid partner " + item.getClass().getName());
+        }
       return documentMap;
     }
     
