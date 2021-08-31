e03106dacffe4c818dfccaab332f9ce6ca93b3f5
Raj Kamal Mitra <rmitra@evolving.com>
2021-08-31 14:47:10 +0530

    get back ssl error

--- evolution/src/main/resources/bin/evolution-deploy-launch-postamble.sh
+++ evolution/src/main/resources/bin/evolution-deploy-launch-postamble.sh
@@ -4,7 +4,4 @@
 #
 #########################################
 
-docker stack deploy -c $DEPLOY_ROOT/stack/stack-gui-ssl-monitoring.yml ${DOCKER_STACK}-gui-ssl-monitoring
-echo "sleeping for 5s before starting gui"
-sleep 5
 docker stack deploy -c $DEPLOY_ROOT/stack/stack-gui.yml ${DOCKER_STACK}-gui
--- evolution/src/main/resources/bin/evolution-launch-postamble.sh
+++ evolution/src/main/resources/bin/evolution-launch-postamble.sh
@@ -4,7 +4,4 @@
 #
 #########################################
 
-docker stack deploy -c $DEPLOY_ROOT/stack/stack-gui-ssl-monitoring.yml <_DOCKER_STACK_>-gui-ssl-monitoring
-echo "sleeping for 5s before starting gui"
-sleep 5
 docker stack deploy -c $DEPLOY_ROOT/stack/stack-gui.yml <_DOCKER_STACK_>-gui
