eb6abd4d20e97d8fdbc49506b60a3a2b0b082947
Frédéric Duclos <frederic.duclos@lumatagroup.com>
2021-09-21 16:43:56 +0200

    EVPRO-1293

--- evolution/src/main/java/com/evolving/nglm/core/SubscriberIDService.java
+++ evolution/src/main/java/com/evolving/nglm/core/SubscriberIDService.java
@@ -15,6 +15,8 @@ import redis.clients.jedis.exceptions.JedisException;
 import com.google.common.primitives.Longs;
 import com.google.common.primitives.Shorts;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -222,6 +224,10 @@ public class SubscriberIDService
         String subscriberID = null;        
         if (binarySubscriberIDs.get(i) != null)
           {
+            // [0, 1,                0, 1,                          0, 0, 0, 0, 0, 0, 0, 120]
+
+            // <TenantID 2 bytes>    <1 nb subscriberID, 2 bytes>   <effectiveSubscriberID>      
+            
             int sizeModulo = binarySubscriberIDs.get(i).length % 8;
             // after multitenancy, the length is 4 + 8* n before it was 2 + 8 * n
             int tmpTenantID = sizeModulo == 4 ? /*after mutlitenancy*/ Shorts.fromByteArray(Arrays.copyOfRange(binarySubscriberIDs.get(i), 0, 2)) : 1;
@@ -247,6 +253,254 @@ public class SubscriberIDService
 
     return result;
   }
+  
+  
+  /****************************************
+    {
+      "apiVersion": 1,
+      "straightAlternateIDName": "msisdn",
+      "straightAlternateIDValue": "12125550120",
+      "subscriberIDs": [
+        {
+          "reverseAlternateIDs": {
+            "contractID": {
+              "120": "121"
+            },
+            "msisdn": {
+              "120": "12125550120"
+            }
+          },
+          "subscriberID": "120"
+        }
+      ],
+      
+      "licenseCheck": {
+        "raisedDate": 1632233423472,
+        "level": 0,
+        "context": "system",
+        "source": "licensemanager",
+        "detail": "Expires at Sun Jan 16 00:59:59 CET 2022",
+        "type": "license_timelimit"
+      }      
+    }
+  *
+  ****************************************/
+  
+  public JSONObject getRedisSubscriberIDsForPTTTests(String alternateIDName, String alternateIDValue)
+  {
+    /****************************************
+    *
+    *  redis cache index
+    *
+    ****************************************/
+
+    if (Deployment.getAlternateIDs().get(alternateIDName) == null)
+      {
+        JSONObject result = new JSONObject();
+        result.put("error", "unknown alternateID name '" + alternateIDName + "'");
+        return result;
+      }
+    
+    /****************************************
+    *
+    *  use with sharedIDs not meaningful
+    *
+    ****************************************/
+
+    if (Deployment.getAlternateIDs().get(alternateIDName).getSharedID())
+      {
+        JSONObject result = new JSONObject();
+        result.put("error", "service not available for shared id '" + alternateIDName + "'");
+        return result;
+      }
+    
+    // retrieve Subscriber ID from alternate ID
+    Map<String, String> subscriberIDsFromOneAlternateID =  getMappingValueAlternateIDToSubcriberIDRedisIndex(alternateIDName, alternateIDValue);
+    
+    if(subscriberIDsFromOneAlternateID != null)
+      {
+        JSONObject result = new JSONObject();
+        JSONArray subscriberIDs = new JSONArray();
+        result.put("straightAlternateIDName", alternateIDName);
+        result.put("straightAlternateIDValue", alternateIDValue);
+        result.put("subscriberIDs", subscriberIDs);
+        
+        for(Map.Entry<String, String> current : subscriberIDsFromOneAlternateID.entrySet()) // <msisdn>,<subscriberID>
+          {           
+            if("tenantID".equals(current.getKey())) { continue; }
+            String foundSubscriberID = current.getValue();
+            JSONObject alternateIDsMap = new JSONObject();
+            //Pair<String, Map<String, Map<String, String>>> oneSubscriberID = new Pair<String, Map<String,Map<String,String>>>(foundSubscriberID, alternateIDsMap);
+            for(String alternateID : Deployment.getAlternateIDs().keySet())
+              {
+                // <SubscriberID> => <alternateIDValue>
+                Map<String, String> alternateIdsFromOneSubscriber = getMappingValueSubscriberIDToAlternateIDRedisIndex(alternateID, foundSubscriberID); //
+               
+                if(alternateIdsFromOneSubscriber != null) 
+                  {
+                    JSONObject reverseAlternateID = new JSONObject();
+                    alternateIDsMap.put(alternateID, reverseAlternateID);
+                    JSONObject oneReverse = new JSONObject();
+                    oneReverse.putAll(alternateIdsFromOneSubscriber);
+                    reverseAlternateID.put("reverse", oneReverse);
+                    
+                    Map<String, String> straight = getMappingValueAlternateIDToSubcriberIDRedisIndex(alternateID, alternateIdsFromOneSubscriber.get(foundSubscriberID));
+                    if(straight != null && straight.size() > 0) {
+                      JSONObject straightJSON = new JSONObject();
+                      straightJSON.putAll(straight);
+                      reverseAlternateID.put("straight", straight);                      
+                    }
+                  }
+              }
+            JSONObject arrayElement = new JSONObject();
+            arrayElement.put("subscriberID", foundSubscriberID);
+            arrayElement.put("alternateIDs", alternateIDsMap);
+            subscriberIDs.add(arrayElement);
+          }
+        return result;        
+      }
+    else 
+      {
+        JSONObject result = new JSONObject();
+        result.put("error", "no subscriberID for " + alternateIDName +  " " + alternateIDValue);
+        return result;
+      }
+  }
+
+  private Map<String, String> getMappingValueAlternateIDToSubcriberIDRedisIndex(String alternateIDName, String alternateIDValue)
+  {
+    
+    List<byte[]> binaryAlternateIDs = new ArrayList<byte[]>();
+    
+    binaryAlternateIDs.add(alternateIDValue.getBytes(StandardCharsets.UTF_8));
+    int redisCacheIndex = Deployment.getAlternateIDs().get(alternateIDName).getRedisCacheIndex();
+    
+    /****************************************
+    *
+    *  retrieve from redis
+    *
+    ****************************************/
+    
+    BinaryJedis jedis = jedisSentinelPool.getResource();
+    List<byte[]> retrievedBinaryIDs = null;
+    try
+      {
+        jedis.select(redisCacheIndex);
+        retrievedBinaryIDs = jedis.mget(binaryAlternateIDs.toArray(new byte[0][0]));
+      }
+    catch (JedisException e)
+      {
+        //
+        //  log
+        //
+
+        log.error("JEDIS error");
+        StringWriter stackTraceWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stackTraceWriter, true));
+        log.error(stackTraceWriter.toString());
+
+        //
+        //  abort
+        //
+
+        return null;
+      }
+    finally
+      {
+        try { jedis.close(); } catch (JedisException e1) { }
+      }
+
+    /****************************************
+    *
+    *  extract from binaries
+    *
+    ****************************************/
+
+    Map<String,String> result = new HashMap<String,String>();
+    for (int i = 0; i < binaryAlternateIDs.size(); i++)
+      {
+        String keyInRedis = new String(binaryAlternateIDs.get(i), StandardCharsets.UTF_8);
+        
+        String valueInRedis = null;        
+        if (retrievedBinaryIDs.get(i) != null)
+          {
+            int sizeModulo = retrievedBinaryIDs.get(i).length % 8;
+            // after multitenancy, the length is 4 + 8* n before it was 2 + 8 * n
+            int tmpTenantID = sizeModulo == 4 ? /*after mutlitenancy*/ Shorts.fromByteArray(Arrays.copyOfRange(retrievedBinaryIDs.get(i), 0, 2)) : 1;
+            short numberOfSubscriberIDs = sizeModulo == 4 ? /* after multitenancy */ Shorts.fromByteArray(Arrays.copyOfRange(retrievedBinaryIDs.get(i), 2, 4)) : Shorts.fromByteArray(Arrays.copyOfRange(retrievedBinaryIDs.get(i), 0, 2));
+            if (numberOfSubscriberIDs > 1) throw new RuntimeException("invariant violated - multiple subscriberIDs");
+            valueInRedis = (numberOfSubscriberIDs == 1) ? (sizeModulo == 4 ? Long.toString(Longs.fromByteArray(Arrays.copyOfRange(retrievedBinaryIDs.get(i), 4, 12))) : Long.toString(Longs.fromByteArray(Arrays.copyOfRange(retrievedBinaryIDs.get(i), 2, 10))) ) : null;
+            result.put(keyInRedis, valueInRedis);
+            result.put("tenantID", ""+tmpTenantID);
+          }
+      }
+    return result;
+  }
+  
+  private Map<String, String> getMappingValueSubscriberIDToAlternateIDRedisIndex(String alternateIDName, String subscriberID)
+  {
+    
+    List<byte[]> binaryAlternateIDs = new ArrayList<byte[]>();
+    
+    binaryAlternateIDs.add(Longs.toByteArray(Long.parseLong(subscriberID)));
+    int redisCacheIndex = Deployment.getAlternateIDs().get(alternateIDName).getReverseRedisCacheIndex();
+
+    /****************************************
+    *
+    *  retrieve from redis
+    *
+    ****************************************/
+    
+    BinaryJedis jedis = jedisSentinelPool.getResource();
+    List<byte[]> retrievedBinaryIDs = null;
+    try
+      {
+        jedis.select(redisCacheIndex);
+        retrievedBinaryIDs = jedis.mget(binaryAlternateIDs.toArray(new byte[0][0]));
+      }
+    catch (JedisException e)
+      {
+        //
+        //  log
+        //
+
+        log.error("JEDIS error");
+        StringWriter stackTraceWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stackTraceWriter, true));
+        log.error(stackTraceWriter.toString());
+
+        //
+        //  abort
+        //
+
+        return null;
+      }
+    finally
+      {
+        try { jedis.close(); } catch (JedisException e1) { }
+      }
+
+    /****************************************
+    *
+    *  extract from binaries
+    *
+    ****************************************/
+
+    Map<String,String> result = new HashMap<String,String>();
+    for (int i = 0; i < binaryAlternateIDs.size(); i++)
+      {
+        String keyInRedis = "" + Longs.fromByteArray(binaryAlternateIDs.get(i));
+        
+        String valueInRedis = null;        
+        if (retrievedBinaryIDs.get(i) != null)
+          {
+            valueInRedis = new String(retrievedBinaryIDs.get(i), StandardCharsets.UTF_8);
+            result.put(keyInRedis, valueInRedis);
+          }
+      }
+    return result;
+  }
+
 
   /****************************************
   *
