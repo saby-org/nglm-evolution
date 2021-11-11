04dd53628ec808025e75d1769111dd999d030743
daniel cioloca <daniel.cioloca@evolving.com>
2021-11-11 14:15:36 +0200

    EVPRO-1386 removedPrefix for incoming mosms event

--- evolution/src/main/java/com/evolving/nglm/evolution/MONotificationEvent.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/MONotificationEvent.java
@@ -5,4 +5,7 @@ import java.util.Date;
 public interface MONotificationEvent
 {
   public void fillWithMOInfos(String subscriberID, Date originTimesTamp, String channelName, String sourceAdress, String destinationAddress, String messageText);
+  default public String formatAddressForRedis (String address){
+    return address;
+  }
 }
