7640a8f1979dd9cbc8e6f6271d8fe1b391739feb
Raj Kamal Mitra <rmitra@evolving.com>
2021-11-12 17:09:41 +0530

    BADGE_NOT_REMOVABLE fix

--- evolution/src/main/java/com/evolving/nglm/evolution/EvolutionEngine.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/EvolutionEngine.java
@@ -2821,7 +2821,6 @@ public class EvolutionEngine
        switch (actionRequest)
        {
          case AWARD:
-           boolean triggerWorkflow = badgeChangeRequest.getInfos().get("") == null ? true : (boolean) badgeChangeRequest.getInfos().get("");
            if (subscriberBadge == null)
              {
                //
@@ -9562,10 +9561,10 @@ public class EvolutionEngine
 
       BadgeChange badgeChangeRequest = new BadgeChange(evolutionEventContext.getSubscriberState().getSubscriberID(), uniqueKeyServer.getKey(), evolutionEventContext.getEventID(), operation, badgeID, moduleID, deliveryRequestSource, origin, RESTAPIGenericReturnCodes.SUCCESS, evolutionEventContext.getSubscriberState().getSubscriberProfile().getTenantID(), new ParameterMap());
       boolean changed = changeSubscriberBadge(badgeChangeRequest, evolutionEventContext.getSubscriberState(), evolutionEventContext.processingDate(), true);
+      badgeChangeRequest.getInfos().put("executeWorkFlowOnly", changed);
       if (changed)
         {
           subscriberEvaluationRequest.getJourneyState().getBadgeChanges().add(badgeChangeRequest);
-          badgeChangeRequest.getInfos().put("executeWorkFlowOnly", changed);
         }
 
       /*****************************************
