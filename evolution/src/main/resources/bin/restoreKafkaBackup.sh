7c824161d4481b6d526a24c7ff502bade297b0dd
Marc Kossa <marc.kossa@evolving.com>
2021-12-01 17:07:36 +0100

    EVPRO-887

--- evolution/src/main/resources/bin/restoreKafkaBackup.sh
+++ evolution/src/main/resources/bin/restoreKafkaBackup.sh
@@ -1,44 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+#################################################################################
+#
 # restoreKafkaBackup.sh
+#
+#################################################################################
+
+#
+#  sets
+#
+
+set -o errexit \
+    -o verbose \
+    -o xtrace
 
 echo "***************************************************************************************************"
 echo "This script will allow you to restore kafka topics, from data saved by the backupmanager container."
 echo "***************************************************************************************************"
 echo
 
-rep=""
-while [ "$rep" == "" ]
-do
-  echo -n "--> Backup file : "
-  read rep
-done
-
-BACKUPFILE=$rep
-if [ ! -f $BACKUPFILE ]; then
-   echo "file does not exist, exiting"
-   exit 1
+if [ $# -lt 2 ]
+then
+  echo "Usage : restoreKafkaBackup.sh <backupfile> <topic> [<logLevel>]"
+  echo "logLevel is optional, defaults to INFO, possible values : ERROR WARN INFO DEBUG TRACE"
+  exit 1
 fi
 
-rep=""
-while [ "$rep" == "" ]
-do
-  echo -n "--> Topic name : "
-  read rep
-done
+TOPIC=$2
+LOGLEVEL=$3
+if [ -z $LOGLEVEL ]; then
+	LOGLEVEL=INFO
+fi
 
-TOPIC=$rep
-
-rep=""
-while [ "$rep" == "" -a "$r" != "y" -a "$r" != "n" ]
-do
-  echo -n "We will restore $TOPIC from data in $BACKUPFILE, is that correct (y/n) ? "
-  read rep
-done
-
-if [ "$rep" != "y" ]; then
-  echo "Exiting"
-  exit 1
+BACKUPFILE=$1
+if [ ! -f $BACKUPFILE ]; then
+   echo "Backup file $BACKUPFILE does not exist"
+   if [ -z $ENTRYPOINT ]; then # this is only defined in evolution containers
+    	echo "Try to execute remotely on the ev-backupmanager container"
+   		docker container exec `docker ps -aqf "name=ev-backupmanager"` /app/bin/restoreKafkaBackup.sh $BACKUPFILE $TOPIC $LOGLEVEL
+   		exit 0
+   else
+   		exit 1
+   fi
 fi
 
 export BACKUP_LOGFILE=/tmp/log.$$.xml
@@ -51,32 +54,32 @@ cat <<EOF
 		<param name="Target" value="System.out" />
 		<layout class="org.apache.log4j.EnhancedPatternLayout">
 			<param name="ConversionPattern"
-				value="%d{ISO8601}{<_DEPLOYMENT_TIMEZONE_>} %p [%t] %m%n" />
+				value="%d{ISO8601}{$DEPLOYMENT_TIMEZONE} %p [%t] %m%n" />
 		</layout>
 	</appender>
 
 	<category name="com.evolving.nglm">
-		<priority value="ERROR" />
+		<priority value="$LOGLEVEL" />
 	</category>
 
 	<category name="com.evolving.nglm.evolution.backup.kafka">
-		<priority value="INFO" />
+		<priority value="$LOGLEVEL" />
 	</category>
 
 	<root>
-		<priority value="ERROR" />
+		<priority value="$LOGLEVEL" />
 		<appender-ref ref="stdout" />
 		<appender-ref ref="eventAppender" />
 	</root>
 
 	<category name="org.apache.kafka">
-		<priority value="ERROR" />
+		<priority value="$LOGLEVEL" />
 	</category>
 	<category name="io.confluent">
-		<priority value="ERROR" />
+		<priority value="$LOGLEVEL" />
 	</category>
 	<category name="org.apache.zookeeper">
-		<priority value="ERROR" />
+		<priority value="$LOGLEVEL" />
 	</category>
 
 </log4j:configuration>
@@ -84,10 +87,10 @@ cat <<EOF
 EOF
 ) > $BACKUP_LOGFILE
 
-java -cp ${project.build.directory}/lib/*:${project.build.directory}/${project.artifactId}-${project.version}.jar:$CONFLUENT_HOME/share/java/kafka/* \
-    -Dnglm.schemaRegistryURL=<_REGISTRY_URL_> -Dnglm.converter=Avro \
-    -Dzookeeper.connect=<_ZOOKEEPER_SERVERS_> -Dbroker.servers=<_BROKER_SERVERS_> \
+java -cp /app/jars/*:/usr/share/java/kafka/* \
+    -Dnglm.schemaRegistryURL=$REGISTRY_URL -Dnglm.converter=Avro \
+    -Dzookeeper.connect=$ZOOKEEPER_SERVERS -Dbroker.servers=$BROKER_SERVERS \
     -Dnglm.zookeeper.root=${zookeeper.root} -Dlog4j.configuration=file:$BACKUP_LOGFILE \
-    -Dredis.sentinels=<_REDIS_SENTINELS_> -Dsubscriberprofile.endpoints=<_EVOLUTIONENGINE_SUBSCRIBERPROFILE_ENDPOINTS_> \
+    -Dredis.sentinels=$REDIS_SENTINELS -Dsubscriberprofile.endpoints=$EVOLUTIONENGINE_SUBSCRIBERPROFILE_ENDPOINTS \
     com.evolving.nglm.evolution.backup.kafka.BackupTopic $TOPIC $BACKUPFILE 
 
