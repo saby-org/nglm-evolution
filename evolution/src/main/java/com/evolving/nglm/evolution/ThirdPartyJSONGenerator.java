078741243b704827a6abc2d1560c5b9476be14ef
Roshnee PERUMAL <roshnee.perumal@evolving.com>
2021-12-17 08:55:56 +0100

    EVPRO-1478

--- evolution/src/main/java/com/evolving/nglm/evolution/ThirdPartyJSONGenerator.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/ThirdPartyJSONGenerator.java
@@ -191,8 +191,6 @@ public class ThirdPartyJSONGenerator
 
                               }
                             offerObjectiveJSON.put("catalogCharacteristics", newCatalogCharateristics);
-
-                            offerObjectives.add(offerObjectiveJSON);
                           }
                       }
                     offerObjectives.add(offerObjectiveJSON);
