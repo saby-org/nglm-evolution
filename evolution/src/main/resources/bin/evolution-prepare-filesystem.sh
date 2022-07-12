265d139b83d61ecbfc52f5cf0a003f0a5b3a42c0
Priyajit <priyajit1995@gmail.com>
2022-07-13 01:07:50 +0530

    .

--- evolution/src/main/resources/bin/evolution-prepare-filesystem.sh
+++ evolution/src/main/resources/bin/evolution-prepare-filesystem.sh
@@ -88,7 +88,11 @@ do
    cat $DEPLOY_ROOT/config/logger/log4j-thirdpartyevent.xml | perl -e 'while ( $line=<STDIN> ) { $line=~s/<_([A-Z_0-9]+)_>/$ENV{$1}/g; print $line; }' | sed 's/\\n/\n/g' | sed 's/^/  /g' > $DEPLOY_ROOT/config/logger/log4j-thirdpartyevent-$KEY.xml
    scp $DEPLOY_ROOT/config/logger/log4j-thirdpartyevent-$KEY.xml $HOST:$NGLM_CONFIG_LOGS/log4j-thirdpartyevent-$KEY.xml
    rm -f $DEPLOY_ROOT/config/logger/log4j-thirdpartyevent-$KEY.xml
-   scp $DEPLOY_ROOT/config/resources/thirdpartymanager.pem $HOST:$NGLM_ROOT/config/thirdpartymanager.pem
+   
+   ssh $HOST "
+      mkdir -p $NGLM_CONFIG_CERTIFICATE      
+   " 
+   scp $DEPLOY_ROOT/certificates/thirdpartymanager.pem $HOST:$NGLM_CONFIG_CERTIFICATE/thirdpartymanager.pem
  done
 
  #
