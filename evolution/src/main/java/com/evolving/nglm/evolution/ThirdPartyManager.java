1d5cd94fe1d1d5c99d32e0c88b59054815b9133f
Raj Kamal Mitra <rmitra@evolving.com>
2022-08-09 13:35:31 +0530

    .

--- evolution/src/main/java/com/evolving/nglm/evolution/ThirdPartyManager.java
+++ evolution/src/main/java/com/evolving/nglm/evolution/ThirdPartyManager.java
@@ -5187,6 +5187,7 @@ public class ThirdPartyManager
     
     List<QueryBuilder> filters = new ArrayList<QueryBuilder>();
     filters.add(QueryBuilders.matchQuery("deliveryRequestID", deliveryRequestID));
+    filters.add(QueryBuilders.matchQuery("returnCode", PurchaseFulfillmentStatus.PURCHASED.getReturnCode()));
     
     //
     //  get from elasticsearch
